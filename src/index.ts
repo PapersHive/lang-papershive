629769815aaa51cfca2b961e79f68359983baa63
Anna Nilsson <anna@nonorank.com>
2021-06-17 15:05:35 +0200

    Updated info based on papershive

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import {parser} from "./syntax.grammar"
-import {LezerLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@codemirror/highlight"
+import { parser } from "./syntax.grammar";
+import {
+  LezerLanguage,
+  LanguageSupport,
+  indentNodeProp,
+  foldNodeProp,
+  foldInside,
+  delimitedIndent
+} from "@codemirror/language";
+import { styleTags, tags as t } from "@codemirror/highlight";
 
-export const EXAMPLELanguage = LezerLanguage.define({
+export const papershiveLanguage = LezerLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
+        Application: delimitedIndent({ closing: ")", align: false })
       }),
       foldNodeProp.add({
         Application: foldInside
@@ -21,10 +28,10 @@ export const EXAMPLELanguage = LezerLanguage.define({
     ]
   }),
   languageData: {
-    commentTokens: {line: ";"}
+    commentTokens: { line: ";" }
   }
-})
+});
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export function papershive() {
+  return new LanguageSupport(papershiveLanguage);
 }
